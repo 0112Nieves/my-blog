757f6f3f837985144b4db5c2bc6e44d0f15b958a
楊子萱 <930112ab@gmail.com>
2026-04-07 19:30:01 +0800

    fix: rss.xml.js site problem

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,8 +4,10 @@ export async function GET(context) {
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
-    site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    site: 'https://nieves-blog.netlify.app',
+    items: await pagesGlobToRssItems(
+      import.meta.glob('./posts/*.md'),
+    ),
     customData: `<language>en-us</language>`,
   });
 }
